b2e1b3cb78d548576c3ef3c7030bb2b90200b88e
ChristianJaiki <christian.programacion12@gmail.com>
2023-10-29 17:27:53 +0100

    ccc

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,13 +9,45 @@ const Navbar = () => {
             <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698587890/61_sin_t__tulo_20231027173013-removebg-preview_480_xoxrqv.png" alt="Logo de tu empresa" />
             </div>
             <div className={`nav_items ${isOpen && "open"}`}>
-                <a href="#">Quienes somos</a>
-                <a href="#">¿Que es el Parkinson?</a>
-                <a href="#">Servicios</a>
-                <a href="#">Actualidad</a>
-                <a href="#">Colabora</a>
-                <a href="#">Trabaja con nosotros</a>
-                <a href="#">Donde encontrarnos</a>
+                <ul className="menu-horizontal">
+                <li><a href="#">Quienes somos</a></li>
+                <li>
+                    <a href="#">¿Que es el Parkinson?</a>
+                    <ul className="menu-vertical">
+                        <li><a href="#">Descripcion</a></li>
+                        <li><a href="#">Descripcion</a></li>
+                    </ul>
+                    </li>
+                <li>
+                    <a href="#">Servicios</a>
+                    <ul className="menu-vertical">
+                        <li><a href="#">Descripcion</a></li>
+                        <li><a href="#">Descripcion</a></li>
+                    </ul>
+                    </li>
+                <li>
+                    <a href="#">Actualidad</a>
+                    <ul className="menu-vertical">
+                        <li><a href="#">Descripcion</a></li>
+                        <li><a href="#">Descripcion</a></li>
+                    </ul>
+                    </li>
+                <li>
+                    <a href="#">Colabora</a>
+                    <ul className="menu-vertical">
+                        <li><a href="#">Descripcion</a></li>
+                        <li><a href="#">Descripcion</a></li>
+                    </ul>
+                    </li>
+                <li>
+                    <a href="#">Trabaja con nosotros</a>
+                    <ul className="menu-vertical">
+                        <li><a href="#">Descripcion</a></li>
+                        <li><a href="#">Descripcion</a></li>
+                    </ul>
+                    </li>
+                <li><a href="#">Donde encontrarnos</a></li>
+                </ul>
             </div>
             <div className={`nav_toggle ${isOpen && "open"}`} onClick={ ()  => setIsOpen(!isOpen)}>
                 <span></span>
