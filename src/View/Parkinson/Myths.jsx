7887259723d1ef37a0eaf7f2865d7c6f8a56a19c
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-10-30 19:29:59 +0100

    cardmyths

--- src/View/Parkinson/Myths.jsx
+++ src/View/Parkinson/Myths.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
-import '../Parkinson/Myths.css';
+import "../Parkinson/Myths.css";
 
 const Myths = () => {
   return (
     <>
       <Navbar />
-     
-        <div className="card">
-        <div className="front">
-          <h1>A</h1>
+
+      <div className="card">
+        <div className="card_landing">
+          <h1>Es una enfermedad contagiosa y hereditaria</h1>
         </div>
-        <div className="back">
-          <h1>A</h1>
+        <div className="card_info">
+          <h2 className="title">La enfermedad no es contagiosa y solo afecta a alrededor del 10-15% de los pacientes que tienen antecedentes familiares de la enfermedad
+          </h2>     
         </div>
       </div>
-     
+
       
     </>
   );
