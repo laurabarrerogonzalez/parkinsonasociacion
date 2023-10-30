76d076a1edcdb9fbac9f91d13e5cf8d85cd649af
ChristianJaiki <christian.programacion12@gmail.com>
2023-10-30 23:31:57 +0100

    ChangeNavbarCSS

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import Navbar from "../../Components/Navbar/Navbar";
+import React from 'react';
+import "../Home/Home.css"
+import Banner from '../../Components/Banner/Banner';
+import Navbar from '../../Components/Navbar/Navbar';
 
-  
+
 const Home = () => {
-    
-    return(
-        <>
-        <Navbar/>
-       <h1>Home</h1> 
-        </>
-
-    )
-       
-}
+  return (
+    <div>
+      <Navbar />
+      <Banner
+        image="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698664674/ai-generated-8273256_1280_dspoq5.jpg"
+        text="Trazando juntos un camino de superación: Acompañando la lucha contra el Parkinson con determinación y esperanza."
+        
+      />
+      {/* Contenido específico de la vista de Home*/}
+    </div>
+  );
+};
 
 export default Home;
