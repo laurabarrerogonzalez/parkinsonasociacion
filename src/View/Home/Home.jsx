e8952db2d8201767cca4a93aaf6237d74d548b4b
ChristianJaiki <christian.programacion12@gmail.com>
2023-10-31 12:52:12 +0100

    Changeeeo

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -11,14 +11,23 @@ const Home = () => {
       <Banner
         image="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698743700/CALENDARIO_2024png_ysfcbp.png"
         text="Trazando juntos un camino de superación: Acompañando la lucha contra el Parkinson con determinación y esperanza."
-        />
+      />
 
-        <div className='Title'>
-        <h1>Hola</h1>
+      <div className='section_title'>
+        <div className='title_home'>
+          <h1>La fuerza de un corazón, <br />
+            la esperanza de un mañana:</h1>
+            <br />
+            <p>"Abrazando el Parkinson con amor y valentía."</p>
         </div>
+        <div className='title_hometwo'>
+          <p>"Enfrentando el Parkinson con valentía, cada día es un paso hacia la fortaleza. Tu determinación es tu mayor poder. ¡Sigamos adelante juntos!</p>
+
+        </div>
+      </div>
       {/* Contenido específico de la vista de Home*/}
     </div>
-    
+
   );
 };
 
