3f15efbc4c8e0ee55d64c8e495c911a9eb65c518
Rodrigo <ramunozg@gmail.com>
2023-11-02 11:37:09 +0100

    changee

---
+++ src/View/Find us/Find us.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Find us/Find us.css"
+import Banner from '../../Components/Banner/Banner';
+import Navbar from "../../Components/Navbar/Navbar";
+  
+const Find = () => {
+    return (
+      <div>
+        <Navbar />
+        <Banner
+          image="https://res.cloudinary.com/doft9ylq1/image/upload/v1698743768/istockphoto-673695060-612x612_cap7ex.webp"
+        />
+  </div>
+     
+  
+    );
+  };
+
+export default Find;
