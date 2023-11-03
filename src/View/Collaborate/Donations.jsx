82c3c5cfbf69833be2f2aaca7970ece05e3fc578
isabellozano37 <isabeloficial37@gmail.com>
2023-11-04 00:07:26 +0100

    FlipBook

--- src/View/Collaborate/Donations.jsx
+++ src/View/Collaborate/Donations.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../assets/Components/Footer/Footer";
+import BannerViews from "../../Components/BannerViews/BannerViews";
+import FlipBook from "../../Components/FlipBook/FlipBook";
 
-  
 const Donations = () => {
-    
-    return(
-        <>
-        <Navbar/>
-       <h1>Donaciones</h1> 
-        </>
-
-    )
-       
-}
+  return (
+    <>
+      <div>
+        <Navbar />
+        <BannerViews
+          image="https://res.cloudinary.com/dit2zhtwz/image/upload/v1698744222/pareja-alegre-celebracion-icono-votacion-sentado-sofa_53876-24976_kke7zn.jpg"
+          title="Mitos y Verdades"
+        />
+      </div>
+      <FlipBook/>
+      <Footer />
+    </>
+  );
+};
 
 export default Donations;
