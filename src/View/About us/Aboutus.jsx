3a7a5c9d1bdc04077b0327d3b07389b03c31d2e3
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-03 02:30:00 +0100

    FinishView

--- src/View/About us/Aboutus.jsx
+++ src/View/About us/Aboutus.jsx
@@ -76,61 +76,65 @@ const Aboutus = () => {
                 </div>
             </section>
 
-            <section className="section_workers">
-                <div className="card_worker">
+            <section className="section_workers" >
+                <div className="card_worker" id="card1">
                     <div className="section_img_worker">
                         <img className="img_worker" src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698965836/descarga_2_nltw4k.jpg" alt="" />
+                        <div class="triangle"></div>
                     </div>
                     <div className="section_content_worker">
-                        <h4>"En cada paso que damos juntos, encontramos la fuerza para superar los desafíos del Parkinson.
-                            ¡Sigamos avanzando con determinación y esperanza!"</h4>
+                        <p>"En cada paso que damos juntos, encontramos la fuerza para superar los desafíos del Parkinson.
+                            ¡Sigamos avanzando con determinación y esperanza!"</p>
 
                         <p className="name_worker">Paloma ruiz</p>
-                        <p>Encargada Centro de día</p>
+                        <p className="name_worker_two">(Encargada Centro de día)</p>
 
                     </div>
 
                 </div>
-                <div className="card_worker">
+                <div className="card_worker" id="card2">
                     <div className="section_img_worker" >
                         <img className="img_worker" src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698965749/Angel_zwozbd.png" alt="" />
+                        <div class="triangle" id="triangle2"></div>
                     </div>
                     <div className="section_content_worker">
-                        <h4>"Nuestro compromiso es más fuerte que cualquier temblor.
-                            ¡Trabajemos unidos, inspirando coraje y alegría en cada momento!"</h4>
+                        <p>"Nuestro compromiso es más fuerte que cualquier temblor.
+                            ¡Trabajemos unidos, inspirando coraje y alegría en cada momento!"</p>
 
                         <p className="name_worker">Angel Serrano</p>
-                        <p>Encargado de rehabilitación</p>
+                        <p className="name_worker_two">(Encargado de rehabilitación)</p>
 
                     </div>
                 </div>
-                <div className="card_worker">
+                <div className="card_worker" id="card4" >
                     <div className="section_content_worker">
-                        <h4>"En esta asociación, la perseverancia es nuestra bandera. Recordemos siempre: cada logro, grande o pequeño,
-                            es una victoria en la batalla contra el Parkinson."</h4>
+                        <p>"En esta asociación, la perseverancia es nuestra bandera. Recordemos siempre: cada logro, grande o pequeño,
+                            es una victoria en la batalla contra el Parkinson."</p>
 
                         <p className="name_worker">Celia García</p>
-                        <p>Encargada de estimulación</p>
+                        <p className="name_worker_two">(Encargada de estimulación)</p>
 
                     </div>
                     <div className="section_img_worker">
                         <img className="img_worker" src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698965750/celia_gknqcr.png" alt="" />
+                        <div class="triangle" id="triangle3"></div>
                     </div>
 
 
 
                 </div>
-                <div className="card_worker">
+                <div className="card_worker" id="card2">
                     <div className="section_content_worker">
-                        <h4>"Con cada sonrisa que compartimos, iluminamos el camino hacia la superación. Juntos,
-                            creamos un espacio de apoyo, empatía y fortaleza para vencer al Parkinson."</h4>
+                        <p>"Con cada sonrisa que compartimos, iluminamos el camino hacia la superación. Juntos,
+                            creamos un espacio de apoyo, empatía y fortaleza para vencer al Parkinson."</p>
 
                         <p className="name_worker">Marcos Chotsourian</p>
-                        <p>Encargado ayuda a domicilio</p>
+                        <p className="name_worker_two">(Encargado ayuda a domicilio)</p>
 
                     </div>
                     <div className="section_img_worker">
                         <img className="img_worker" src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698965857/1696600529245_2_cdnn1v.jpg" alt="" />
+                        <div class="triangle" id="triangle4"></div>
                     </div>
 
                 </div>
