fe80050802c87dbde62cc42da398df3f98760dd8
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-06 09:09:13 +0100

    ñññññññ

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Banner
-        image="https://res.cloudinary.com/dp7lr71t8/image/upload/v1698743700/CALENDARIO_2024png_ysfcbp.png"
+        image="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699219864/zyro-image_wkxrbc.png"
         title="VIVIENDO CON PASIÓN"
         text="Trazando juntos un camino de superación: Acompañando la lucha contra el Parkinson con determinación y esperanza."
       />
