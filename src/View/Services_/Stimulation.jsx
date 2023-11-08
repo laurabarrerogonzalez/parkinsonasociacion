b16ed6fd45dd213f8e2007cc5d685a222cf13319
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-08 10:13:35 +0100

    ddddd

--- src/View/Services_/Stimulation.jsx
+++ src/View/Services_/Stimulation.jsx
@@ -17,21 +17,21 @@ const Stimulation = () => {
                 <div className="title_stimulation">
 
                     <h1>Estimulacion Cognitiva <br />
-                    "¡Activa tu mente, expande tu mundo!"
+                        "¡Activa tu mente, expande tu mundo!"
                     </h1>
 
-                    <p>La estimulación cognitiva es un conjunto de actividades diseñadas para 
+                    <p>La estimulación cognitiva es un conjunto de actividades diseñadas para
                         fortalecer las habilidades mentales, como la memoria y la concentración.
-                         Estos ejercicios benefician a personas de todas 
+                        Estos ejercicios benefician a personas de todas
                         las edades, mejorando la agilidad mental y la calidad de vida.
                     </p>
                     <div className="buttons_items">
                         <button className="button_1_info">
-                        <span>Informacion</span>
+                            <span>Informacion</span>
 
                         </button>
                         <button className="button_2_info">
-                        <span>Registrarse</span>
+                            <span>Registrarse</span>
 
                         </button>
                     </div>
@@ -41,9 +41,21 @@ const Stimulation = () => {
                     <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699356047/Dise%C3%B1o_sin_t%C3%ADtulo_4_1_yll4l4.png" alt="" />
                 </div>
             </section>
-            <Footer/>
-      <ButtonDonate />
-      <ScrollArrow/>
+
+            <section className="ttt">
+            <div class="custom-shape-divider-top-1699433452">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                </svg>
+            </div>
+
+            <p>hhhhhh</p>
+            </section>
+
+
+            <Footer />
+            <ButtonDonate />
+            <ScrollArrow />
 
 
         </div>
