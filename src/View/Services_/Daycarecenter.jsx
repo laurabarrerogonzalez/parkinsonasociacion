b3d314a2051931fedbd6c879eb78da128461e337
Rodrigo <ramunozg@gmail.com>
2023-11-08 13:38:35 +0100

    c

--- src/View/Services_/Daycarecenter.jsx
+++ src/View/Services_/Daycarecenter.jsx
@@ -129,7 +129,7 @@ const Daycarecenter = () => {
   
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <BannerViews
         image="https://res.cloudinary.com/doft9ylq1/image/upload/v1699005153/e96e5008-5541-4c4c-b02c-140d8e7a1183_jwxyis.jpg"
         title="CENTRO DE DÍA"
@@ -213,7 +213,7 @@ const Daycarecenter = () => {
           <div className="image-container2">
             <img
               src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699436691/20200415_112914_iruuon.jpg"
-              alt="Imagen 1"
+              alt="Imagen 4"
               className="activity-image4"
             />
             <img
@@ -232,6 +232,117 @@ const Daycarecenter = () => {
               className="activity-image7"
             />
           </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Daycarecenter; */}
+     <Navbar />
+      <BannerViews
+        image="https://res.cloudinary.com/doft9ylq1/image/upload/v1699005153/e96e5008-5541-4c4c-b02c-140d8e7a1183_jwxyis.jpg"
+        title="CENTRO DE DÍA"
+      />
+      <div className="boxDC1">
+        <div className="title-Centro_dia">
+          <h1>Centro de Día</h1>
+        </div>
+
+        <div className="text_DC">
+          <p>
+          El Centro de Día CON ALMA ofrece una atención integral durante el
+             período diurno a las personas en situación de dependencia, con el objetivo
+             de mantener el mayor grado de autonomía posible. Está dirigido a proporcionar
+             una intervención especializada al usuario de manera que se contemplen
+            y atiendan de un modo integral sus necesidades específicas. Las mismas
+            se cubren desde un enfoque biopsicosocial con servicios de asesoramiento,
+            rehabilitación, orientación para la promoción de la autonomía.
+            en su hogar. <br></br>
+          </p>
+        </div>
+      </div>
+
+      <div className="boxDC2">
+        <div className="title-DC">
+          <h1>Actividades</h1>
+        </div>
+
+        <div className="text-centroD">
+          <ul>
+          <li> <strong>Taller nuevas tecnologías</strong></li>
+     <li> <strong>Deambulación</strong></li>
+     <li> <strong>Hortoterapia</strong></li>
+     <li> <strong>Ludoterapia</strong></li>
+     <li> <strong>Laborterapia</strong></li>
+     <li> <strong>Terapia con perros</strong></li>
+    <li> <strong>Psicomotricidad</strong></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699004873/IMG-20231025-WA0001_qku82e.jpg"
+          alt="Imagen 1"
+          className="activity-image"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699005153/e96e5008-5541-4c4c-b02c-140d8e7a1183_jwxyis.jpg"
+          alt=""
+          className="activity-image1"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699015076/20221110_114455_nsagal.jpg"
+          alt="Imagen 2"
+          className="activity-image2"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699015418/20200626_110630_1_qco2pi.jpg"
+          alt="Imagen 2"
+          className="activity-image3"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699436691/20200415_112914_iruuon.jpg"
+          alt="Imagen 1"
+          className="activity-image4"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699436663/20210810_120627_1_tg9wyh.jpg"
+          alt=""
+          className="activity-image5"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699436643/20200617_113153_zgsadh.jpg"
+          alt="Imagen 2"
+          className="activity-image6"
+        />
+        <img
+          src="https://res.cloudinary.com/doft9ylq1/image/upload/v1699440850/20230131_104420_1_1_chswfw.jpg"
+          alt="Imagen 2"
+          className="activity-image7"
+        />
+      </div>
+
+      <div className="boxDC3">
+        <div className="Centro_dia3">
+          <h1>Servicios Especializados</h1>
+        </div>
+        <div className="text_DC3">
+          <ul>
+          <li> <strong>Atención especializada</strong></li>
+   <li> <strong>Rehabilitación especifica:</strong><br></br>
+    Terapia ocupacional<br></br>
+    Estimulación cognitiva<br></br>
+    Fisioterapia<br></br></li>
+    <li> <strong>Asitencia en las actividades 
+      de la vida diaria</strong></li>
+    <li> <strong>Asesoramiento en materia de ayudas
+      técnicas </strong></li>
+   <li> <strong>Sala multisensorial</strong></li>
+    (Estimulación sensorial)
+          </ul>
         </div>
       </div>
       <Footer />
