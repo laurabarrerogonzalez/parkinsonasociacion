5fad635af7a0bc4149cab406360996beaa82069c
isabellozano37 <isabeloficial37@gmail.com>
2023-11-09 13:09:55 +0100

    Voluntarios

--- src/View/Collaborate/Voluntary.jsx
+++ src/View/Collaborate/Voluntary.jsx
@@ -1,45 +1,15 @@
 import "./Voluntary.css";
-import React, { useEffect } from "react";
+import React from "react";
 import swal from "sweetalert2";
 import Navbar from "../../Components/Navbar/Navbar";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import { red } from "@mui/material/colors";
 import Footer from "../../Components/Footer/Footer";
 import ButtonDonate from "../../Components/ButtonDonate/ButtonDonate";
 import ScrollArrow from "../../Components/ScrollArrow/ScrollArrow";
+import BannerViews from "../../Components/BannerViews/BannerViews";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Voluntary = () => {
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleScroll = () => {
-    const elements = document.querySelectorAll(".services_info");
-    elements.forEach((element) => {
-      const elementTop = element.getBoundingClientRect().top;
-      const elementBottom = element.getBoundingClientRect().bottom;
-
-      const isVisible = elementTop < window.innerHeight && elementBottom >= 0;
-
-      if (isVisible) {
-        element.classList.add("appear");
-      } else {
-        element.classList.remove("appear");
-      }
-    });
-  };
 
   const customColor = "rgb(236, 117, 14)";
 
@@ -66,14 +36,15 @@ const Voluntary = () => {
         }
       });
   };
-
+  
   return (
     <>
       <div className="photos">
         <Navbar />
-        <div className="question">
-          <h1 className="text">¿QUIERES SER VOLUNTARIO?</h1>
-        </div>
+        <BannerViews
+          image="https://res.cloudinary.com/da7ffijqs/image/upload/v1698681283/20201119_125749_1_lmqez1.jpg"
+          title="¿QUIERES SER VOLUNTARIO?"
+        />
         <div className="box1">
           <div className="text1">
             <h1>Objetivos</h1>
@@ -100,128 +71,60 @@ const Voluntary = () => {
           </div>
         </div>
       </div>
-      <div className="forms">
-        <div className="text5">
-          <br />
-          <br />
-          <h1>Solicitud de voluntarios</h1>
-        </div>
-        <div className="container1">
-          <div className="form1">
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "37ch" },
-              }}
-              noValidate
-              autoComplete="off"
-              className="responsive"
-            >
-              <TextField
-                required
-                className="White"
-                id="outlined-required"
-                label="Nombre y Apellidos"
-                defaultValue=""
-              />
-              <br></br>
-              <br></br>
-
-              <TextField
-                required
-                className="White"
-                id="outlined-required"
-                label="Domicilio"
-                defaultValue=""
-              />
-
-              <br></br>
-              <br></br>
-
-              <TextField
-                required
-                className="White"
-                id="outlined-number"
-                label="Telefono"
-                defaultValue=""
-              />
-            </Box>
-          </div>
-          <div className="form2">
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "37ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                required
-                className="White"
-                id="outlined-required"
-                label="Email"
-                type="email"
-                defaultValue=""
-              />
-
-              <br></br>
-              <br></br>
-
-              <TextField
-                required
-                className="White"
-                id="outlined-required"
-                label="Ciudad"
-                defaultValue=""
-              />
-
-              <br></br>
-              <br></br>
-
-              <TextField
-                required
-                className="White"
-                id="outlined-number"
-                label="Provincia"
-                defaultValue=""
-              />
-            </Box>
-            <br />
-            <FormGroup>
-              <FormControlLabel
-                required
-                control={
-                  <Checkbox
-                    {...label}
-                    defaultChecked
-                    sx={{
-                      color: red[800],
-                      "&.Mui-checked": {
-                        color: red[600],
-                      },
-                    }}
-                  />
-                }
-                label="Acepto las Conciciones y la Politica de privacidad"
-              />
-            </FormGroup>
-          </div>
-        </div>
-        <div className="buttonAcept">
-          <Stack direction="row" spacing={2}>
-            <Button
-              style={{ backgroundColor: customColor, color: "#fff" }}
-              onClick={handleEnviarClick}
-            >
-              ENVIAR
-            </Button>
-          </Stack>
-        </div>
+      <form className="form11">
+    <p className="title12">Solicitud Voluntariado</p>
+    <p className="message13">
+      Ayudar es un regalo que se da con el corazón.
+    </p>
+    <div className="flex14">
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="Nombre y apellidos" />
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="DNI" />
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="Fecha de naciomiento" />
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="Domicilio" />
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="Telefono" />
+      <input type="text" autoComplete="off" name="text" className="input14" placeholder="Email" />
+       <input type="text" autoComplete="off" name="text" className="input14" placeholder="Formación" />
+      <div className="radio-inputs">
+        <label>
+          <input className="radio-input" type="radio" name="engine" />
+          <span className="radio-tile">
+            <span className="radio-icon"><img className="iconos1"
+              src="https://res.cloudinary.com/da7ffijqs/image/upload/v1699523977/_63c2f168-6681-4203-a864-def47b6c13e9-modified_gml1c5.png"
+            /></span>
+            <span className="radio-label">Mañanas
+            </span>
+          </span>
+        </label>
+        <label>
+          <input className="radio-input" type="radio" name="engine" defaultChecked />
+          <span className="radio-tile">
+            <span className="radio-icon"><img className="iconos2"
+              src="https://res.cloudinary.com/da7ffijqs/image/upload/v1699523003/_9eb70b3d-1fe2-4adf-a18a-86aa53ea5e6f-removebg-preview_mh6cyo.png"
+            /></span>
+            <span className="radio-label">Tardes</span>
+          </span>
+        </label>
+        <label>
+          <input className="radio-input" type="radio" name="engine" />
+          <span className="radio-tile">
+            <span className="radio-icon"><img className="iconos3"
+              src="https://res.cloudinary.com/da7ffijqs/image/upload/v1699526592/_20e53d9a-7260-4834-81d4-949bc5d96d25-modified_f7l2s1.png"
+            /></span>
+            <span className="radio-label">Fin de Semana</span>
+          </span>
+        </label>
       </div>
-     <Footer/> 
-     <ButtonDonate />
-     <ScrollArrow />
+      <label className="container40">
+        <input defaultChecked type="checkbox" />
+        <div className="checkmark"></div>
+        <div className="Terminos"><p>Acepto las Conciciones y la Politica de privacidad</p></div>
+      </label>
+    </div>
+    <button onClick={handleEnviarClick} className="bn01" type="button">Enviar!</button>
+  </form>
+      <Footer />
+      <ButtonDonate />
+      <ScrollArrow />
     </>
   );
 };
