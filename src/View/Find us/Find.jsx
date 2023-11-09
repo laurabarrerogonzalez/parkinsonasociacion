133dd7cde8ec66fd74c3c99e18d3f2e14fcd9880
miguelcidoncha <cidoncha1996@gmail.com>
2023-11-09 09:30:30 +0100

    add. Map

--- src/View/Find us/Find.jsx
+++ src/View/Find us/Find.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Mapa from "../../Components/Map/Map";
+import BannerViews from "../../Components/BannerViews/BannerViews";
+import Footer from "../../Components/Footer/Footer";
+import ButtonDonate from "../../Components/ButtonDonate/ButtonDonate";
+import ScrollArrow from "../../Components/ScrollArrow/ScrollArrow";
+
 
   
 const Find = () => {
@@ -8,7 +13,13 @@ const Find = () => {
     return(
         <>
         <Navbar/>
+        <BannerViews
+          image="https://res.cloudinary.com/dit2zhtwz/image/upload/v1699193991/istockphoto-532484805-612x612_l9xgr1.jpg"
+          title="Evolución y Tratamiento"/>
         <Mapa/> 
+        <Footer/>
+        <ButtonDonate/>
+        <ScrollArrow/>
         </>
 
     )
