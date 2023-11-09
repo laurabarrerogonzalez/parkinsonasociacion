81cdb0edbbd7315ef6f2fca8db1855d40ce1e4b7
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-09 13:39:22 +0100

    xxxxxxxx

--- src/View/Services_/Stimulation.jsx
+++ src/View/Services_/Stimulation.jsx
@@ -42,14 +42,35 @@ const Stimulation = () => {
                 </div>
             </section>
 
-            <section className="ttt">
-            <div class="custom-shape-divider-top-1699433452">
-                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-                </svg>
-            </div>
+            <section className="section_benefits">
+                <div class="custom-shape-divider-top-1699433452">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                    </svg>
+                </div>
 
-            <p>hhhhhh</p>
+                    <h1>Beneficios</h1>
+                
+
+                <div class="card_benefits">
+                    <p>Memoria</p>
+                    <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699524677/que-es-estimulacion-cognitiva_hrilej.jpg" alt="" />
+                </div>
+                <div class="card_benefits" id="two_benefits">
+                <p>Ánimo</p>
+                    <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699526869/las-personas-mayores-son-mas-felices-que-las-mas-jovenes-950x797_eywa8y.jpg" alt="" />
+                </div>
+                <div class="card_benefits" id="three_benefits">
+                <p>Creatividad</p>
+                    <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699525707/questions-2519654_1280_omdaca.png" alt="" />
+                </div>
+                <div class="card_benefits" id="four_benefits">
+                <p>Deterioro
+                Cognitivo</p>
+                    <img src="https://res.cloudinary.com/dp7lr71t8/image/upload/v1699525954/deterioro-cognitivo_1_621x621_cwdwr2.jpg" alt="" />
+                </div>
+
+
             </section>
 
 
