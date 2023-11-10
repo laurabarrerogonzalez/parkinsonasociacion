d7e3e9379dc951cc3eb4bbd990fa827d130a0473
Rodrigo <ramunozg@gmail.com>
2023-11-10 10:10:01 +0100

    c

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,5 +1,32 @@
+// import React from 'react';
+// import { Calendar, theme } from 'antd';
+
+// const onPanelChange = (value, mode) => {
+//   console.log(value.format('YYYY-MM-DD'), mode);
+// };
+
+// const CalendarComponent = () => {
+//   const { token } = theme.useToken();
+
+//   const wrapperStyle = {
+//     width: 300,
+//     border: `1px solid ${token.colorBorderSecondary}`,
+//     borderRadius: token.borderRadiusLG,
+ 
+//   };
+
+//   return (
+//     <div style={wrapperStyle}>
+//       <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+//     </div>
+//   );
+// };
+
+// export default CalendarComponent;
+
 import React from 'react';
 import { Calendar, theme } from 'antd';
+import { left } from '@popperjs/core';
 
 const onPanelChange = (value, mode) => {
   console.log(value.format('YYYY-MM-DD'), mode);
@@ -12,11 +39,31 @@ const CalendarComponent = () => {
     width: 300,
     border: `1px solid ${token.colorBorderSecondary}`,
     borderRadius: token.borderRadiusLG,
+    padding: '20px',
+    backgroundColor: 'white',
+    margin:'85px',
+    marginTop:'70px',
+    position: 'relative', // Define la posición relativa
+     top: 650, // Mueve el elemento 20 píxeles desde la posición original en la parte superior
+     left: 50, 
+    
+    
+  };
+
+  const calendarStyle = {
+    // Estilos específicos para el calendario
+    // Por ejemplo:
+    backgroundColor: 'lightblue',
+    border: 'none',
   };
 
   return (
     <div style={wrapperStyle}>
-      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+      <Calendar
+        fullscreen={false}
+        onPanelChange={onPanelChange}
+        style={calendarStyle} // Aplica estilos directamente al componente Calendar
+      />
     </div>
   );
 };
