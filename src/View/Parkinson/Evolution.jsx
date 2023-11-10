a78a221adbda82aebfe1904ecde6b3fe8ad5deb2
laurabarrerogonzalez <laura.bgpr94@gmail.com>
2023-11-10 19:28:28 +0100

    Evolutiontimeline-changedclassNamecard-Findus

--- src/View/Parkinson/Evolution.jsx
+++ src/View/Parkinson/Evolution.jsx
@@ -58,19 +58,19 @@ const Evolution = () => {
         <div id="progress-bar-container">
           <ul>
             <li className={`step step01 ${currentStep === 1 ? 'active' : ''}`} onClick={() => handleStepClick(1)}>
-              <span>Step 1</span>
+              <span>Estadío I</span>
             </li>
             <li className={`step step02 ${currentStep === 2 ? 'active' : ''}`} onClick={() => handleStepClick(2)}>
-              <span>Step 2</span>
+              <span>Estadío II</span>
             </li>
             <li className={`step step03 ${currentStep === 3 ? 'active' : ''}`} onClick={() => handleStepClick(3)}>
-              <span>Step 3</span>
+              <span>Estadío III</span>
             </li>
             <li className={`step step04 ${currentStep === 4 ? 'active' : ''}`} onClick={() => handleStepClick(4)}>
-              <span>Step 4</span>
+              <span>Estadío IV</span>
             </li>
             <li className={`step step05 ${currentStep === 5 ? 'active' : ''}`} onClick={() => handleStepClick(5)}>
-              <span>Step 5</span>
+              <span>Estadío V</span>
             </li>
           </ul>
           <div id="line">
@@ -79,142 +79,77 @@ const Evolution = () => {
         </div>
         <div id="progress-content-section">
           <div className={`section-content discovery ${currentStep === 1 ? 'active' : ''}`}>
-            <h2>Section 1</h2>
-            <p>Text for section 1</p>
-          </div>
-          <div className={`section-content strategy ${currentStep === 2 ? 'active' : ''}`}>
-            <h2>Section 2</h2>
-            <p>Text for section 2</p>
-          </div>
-          <div className={`section-content creative ${currentStep === 3 ? 'active' : ''}`}>
-            <h2>Section 3</h2>
-            <p>Text for section 3</p>
-          </div>
-          <div className={`section-content production ${currentStep === 4 ? 'active' : ''}`}>
-            <h2>Section 4</h2>
-            <p>Text for section 4</p>
-          </div>
-          <div className={`section-content analysis ${currentStep === 5 ? 'active' : ''}`}>
-            <h2>Section 5</h2>
-            <p>Text for section 5</p>
-          </div>
-        </div>
-      </div>
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <div className="background-evolution">
-        <div className="timeline">
-          {[0, 1, 2, 3, 4, 5].map((stage) => (
-            <div
-              key={stage}
-              className={`timeline-item stage-${stage}`}
-              onMouseEnter={() => handleMouseEnter(stage)}
-              onMouseLeave={handleMouseLeave}
-            >
-              {stage}
-            </div>
-          ))}
-        </div>
-
-        <div className="description">{stageDescription}</div>
-
-        <div className="grid-layout">
-          <div className="box b1">
-            <h1 className="h1evolution">Diagnóstico reciente</h1>
-          </div>
-          <div className="box b2">
-            <h1 className="h1evolution">Pacientes Moderadamente Afectados</h1>
-          </div>
-          <div className="box b3">
-            <h1 className="h1evolution">Pacientes Severamente Afectados</h1>
-          </div>
-          <div className="box b4">
-            <h2>Estadío I</h2>
-            <li className="li-state">Expresión facial normal</li>
+            <h2>Diagnóstico Reciente</h2>
+            <p>Estadío I</p>
+            <p> <li className="li-state">Expresión facial normal</li>
             <li className="li-state">Postura erecta</li>
             <li className="li-state">Posible temblor en una extremidad</li>
             <li className="li-state">Dificultades movilidad fina</li>
-            <li className="li-state">
-              Rigidez y bradiquinesia a la exploración cuidadosa
-            </li>
-            <li className="li-state">
-              Disminución del braceo al caminar, arrastrando un poco los pies
-            </li>
+            <li className="li-state">Rigidez y bradiquinesia a la exploración cuidadosa</li>
+            <li className="li-state">Disminución del braceo al caminar, arrastrando un poco los pies</li> 
+            </p>
           </div>
-          <div className="box b5">
-            <h2>Estadío II</h2>
-            <li className="li-state">
-              Alteración de la expresión facial. Disminución del parpadeo
-            </li>
+
+          <div className={`section-content strategy ${currentStep === 2 ? 'active' : ''}`}>
+            <h2>Diagnóstico Reciente</h2>
+            <p>Estadío II</p>
+            <p>
+            <li className="li-state">Alteración de la expresión facial. Disminución del parpadeo</li>
             <li className="li-state">Postura en ligera flexión</li>
-            <li className="li-state">
-              Enlentecimiento para realizar las actividades de la vida diaria
-            </li>
+            <li className="li-state">Enlentecimiento para realizar las actividades de la vida diaria</li>
             <li className="li-state">Síntomas depresivos</li>
-            <li className="li-state">
-              Posibilidad de efectos secundarios de los medicamentos
-            </li>
+            <li className="li-state">Posibilidad de efectos secundarios de los medicamentos</li>
+            </p>
           </div>
-          <div className="box b6">
-            <h2>Estadíos III y IV</h2>
-            <li className="li-state">
-              Dificultades al caminar: se acorta el paso, dificultades en los
-              giros
-            </li>
-            <li className="li-state">
-              Dificultades en el equilibrio: caídas, dificultad para pararse
-            </li>
+
+          <div className={`section-content creative ${currentStep === 3 ? 'active' : ''}`}>
+            <h2>Pacientes Moderadamente Afectados</h2>
+            <p>Estadío III</p>
+            <p>
+            <li className="li-state">Dificultades al caminar: se acorta el paso, dificultades en los giros</li>
+            <li className="li-state">Dificultades en el equilibrio: caídas, dificultad para pararse</li>
             <li className="li-state">Sensación de fatiga, dolores</li>
             <li className="li-state">Dificultades comunicativas</li>
             <li className="li-state">Síntomas de disfunción autonómica</li>
-            <li className="li-state">
-              Síntomas de relación con los fármacos: fenómenos on-off (miedo e
-              inseguridad), discinesias y problemas conductuales (insomnio,
-              alucinaciones, cuadros confusionales)
-            </li>
+            <li className="li-state">Síntomas de relación con los fármacos: fenómenos on-off (miedo e inseguridad), discinesias y problemas conductuales (insomnio,
+              alucinaciones, cuadros confusionales)</li>
+            </p>
           </div>
 
-          <div className="box b7">
-            <h2>Estadío V</h2>
-            <li className="li-state">
-              No todos los pacientes llegan a este estadío
-            </li>
+          <div className={`section-content production ${currentStep === 4 ? 'active' : ''}`}>
+            <h2>Pacientes Moderadamente Afectados</h2>
+            <p>Estadío IV</p>
+            <p>
+            <li className="li-state">Dificultades al caminar: se acorta el paso, dificultades en los giros</li>
+            <li className="li-state">Dificultades en el equilibrio: caídas, dificultad para pararse</li>
+            <li className="li-state">Sensación de fatiga, dolores</li>
+            <li className="li-state">Dificultades comunicativas</li>
+            <li className="li-state">Síntomas de disfunción autonómica</li>
+            <li className="li-state">Síntomas de relación con los fármacos: fenómenos on-off (miedo e inseguridad), discinesias y problemas conductuales (insomnio,
+              alucinaciones, cuadros confusionales)</li>
+            </p>
+          </div>
+
+          <div className={`section-content analysis ${currentStep === 5 ? 'active' : ''}`}>
+            <h2>Pacientes Severamente Afectados</h2>
+            <p>Estadío V</p>
+            <p>
+            <li className="li-state">No todos los pacientes llegan a este estadío</li>
             <li className="li-state">Dependientes</li>
-            <li className="li-state">
-              Aumento progresivo del tiempo off. Gran parte del tiempo sentado o
-              en cama
-            </li>
+            <li className="li-state">Aumento progresivo del tiempo off. Gran parte del tiempo sentado o en cama</li>
             <li className="li-state">Trastornos del lenguaje acentuados</li>
             <li className="li-state">Desarrollo de contracturas</li>
-            <li className="li-state">
-              Posibilidad de úlceras de decúbito. Infecciones urinarias de
-              repetición
-            </li>
+            <li className="li-state">Posibilidad de úlceras de decúbito. Infecciones urinarias de repetición</li>
             <li className="li-state">Disfagia progresiva</li>
+            </p>
           </div>
         </div>
       </div>
+      </div>
 
-      <div className="what">
-      <div className="title-parkinson">
-      <h1>Tratamientos</h1>
+      <div className="what1">
+      <div className="title-parkinson1">
+      <h1>Tratamiento</h1>
       </div>
       </div>
 
