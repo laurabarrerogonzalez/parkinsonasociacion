791287a01f037547f6b4c877388428288fd447c7
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-17 11:36:42 +0100

    fdddf

--- src/View/Parkinson/Myths.jsx
+++ src/View/Parkinson/Myths.jsx
@@ -32,12 +32,12 @@ const Myths = () => {
 <div class="cardd">
        <div class="content">
            <div class="front1">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>Es una enfermedad hereditaria</strong></p>
            </div>
 
            <div class="back1">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Solo un pequeño porcentaje (10%-15%) tiene antecedentes familiares. 
                <br></br>
@@ -51,14 +51,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front2">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Es una enfermedad propia de las personas mayores
                 </strong></p>
            </div>
 
            <div class="back2">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Inicio común 50-60 años: 70% diagnósticos con más de 65 años.
                <br></br>
@@ -93,14 +93,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front4">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Produce demencia y pérdida de razonamiento
                 </strong></p>
            </div>
 
            <div class="back4">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Parkinson es distinto del Alzheimer.
                <br></br>
@@ -135,14 +135,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front6">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Todo temblor es Parkinson
                 </strong></p>
            </div>
 
            <div class="back6">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Otras enfermedades también cursan con temblor
                </p>
@@ -154,14 +154,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front7">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Se manifiesta solo con temblores
                 </strong></p>
            </div>
 
            <div class="back7">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Existen otros síntomas motores y no motores que caracterizan la enfermedad
                <br></br>
@@ -175,14 +175,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front8">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                No limita en la realización de las actividades diarias
                 </strong></p>
            </div>
 
            <div class="back8">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Si, en más del 80% de CASOS MODERADOS Y SEVEROS (Invisibles para la sociedad)
                </p>
@@ -194,14 +194,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front9">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Es una enfermedad neurológica menor
                 </strong></p>
            </div>
 
            <div class="back9">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                 NO: es la 2ª enfermedad neurodegenerativa tras el alzheime
                </p>
@@ -213,14 +213,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front10">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Solo existen tratamientos farmacológicos
                 </strong></p>
            </div>
 
            <div class="back10">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Es muy importante combinar el tratamiento con terapias de rehabilitación
                </p>
@@ -232,14 +232,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front11">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Es mortal esta enfermedad
                 </strong></p>
            </div>
 
            <div class="back11">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                A pesar de ser una enfermedad crónica, las personas no mueren a causa del párkinson
                </p>
@@ -251,14 +251,14 @@ const Myths = () => {
    <div class="cardd">
        <div class="content">
            <div class="front12">
-               <h3 class="title">MITO</h3>
+               <h3 class="titles">MITO</h3>
                <p class="subtitle"><strong>
                Nadie puede ayudarme
                 </strong></p>
            </div>
 
            <div class="back12">
-           <h3 class="title">Verdad</h3>
+           <h3 class="titles">Verdad</h3>
                <p class="description">
                Médicos más informados y apoyo de asociaciones benefician a pacientes
                </p>
