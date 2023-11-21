6e692ca656926058a97e634b4269a370b69f74a6
angelserranocorrea <serranocorreaangel@gmail.com>
2023-11-21 18:56:06 +0100

    changes2

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-//  Footer.jsx 
-// import React from 'react';
+ Footer.jsx 
+ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faInstagramSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
