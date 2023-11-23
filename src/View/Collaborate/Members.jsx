b03204f766995fb0b48fb68461d35c6b3bc7ab9a
ChristianJaiki <christian.programacion12@gmail.com>
2023-11-23 11:43:41 +0100

    cxcc

--- src/View/Collaborate/Members.jsx
+++ src/View/Collaborate/Members.jsx
@@ -393,9 +393,8 @@ const Members = () => {
               }
             />
             <div
-              className={`checkmark ${
-                formsData.termsAccepted ? "checked" : ""
-              }`}
+              className={`checkmark ${formsData.termsAccepted ? "checked" : ""
+                }`}
             ></div>
             <div className="Terminos">
               <TermsAndConditions />
